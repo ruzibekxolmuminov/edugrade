5052d57ad4de0602c178094a82b9d640bd8ddc19
ruzibekxolmuminov <kholmuminovruzibek@gmail.com>
2025-12-26 20:38:00 +0500

    start

---
+++ src/main/java/com/example/EdugradeApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdugradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdugradeApplication.class, args);
+	}
+
+}
